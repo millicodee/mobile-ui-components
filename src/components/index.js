501784000c02fb14a15f410b165c40bf4656c8bd
Milica Aleksic <milica.aleksic@united.cloud>
2025-03-19 11:07:41 +0100

    fix import

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Button } from "./button/Button";
-export { default as Input } from "./button/Input";
-import { default as GradientWrapper} from "./GradientWrapper";
-import { default as ScreenWrapper} from "./GradientWrapper";
+export { default as Input } from "./input/Input";
+export { default as GradientWrapper } from "./GradientWrapper";
+export { default as ScreenWrapper } from "./ScreenWrapper";
 export { default as Text } from "./text/Text";
